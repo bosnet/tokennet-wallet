7c41b857d68b02e270c097e53cf40a5c380666eb
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2017-10-01 12:32:23 +0900

    우측 Numpad 특수키 관련 입력 제한 기준 변경

--- src/UiComponents/AmountInput.js
+++ src/UiComponents/AmountInput.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class AmountInput extends Component {
   onKeyDown( $event ) {
     const keyCode = $event.keyCode;
-    const map = [ 8, 37, 38, 39, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 190 ];
+    const BACKSPACE = 8;
+    const HOME = 36;
+    const END = 35;
+    const DELETE = 46;
+    const POINT = 190;
+    const POINT_NUMPAD = 110;
+    const map = [ 37, 38, 39, 40, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, BACKSPACE, HOME, END, DELETE, POINT, POINT_NUMPAD ];
     if( map.indexOf( keyCode ) === -1 ) {
       $event.preventDefault();
     }
